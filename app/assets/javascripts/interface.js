7d42fc677ff15f3da1cb924cd943f4014107a205
johnnydee8 <j.dworzynski@me.com>
2016-10-11 14:34:57 +0100

    fixed js loading, copied bootstrap to resources

---
+++ app/assets/javascripts/interface.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+  var c= document.getElementById("canvas");
+  var ctx = c.getContext("2d");
+  ctx.fillRect(20, 20, 150, 100);
+}
--- interface.js
+++
@@ -1,3 +0,0 @@
-var c= document.getElementById("canvas");
-var ctx = c.getContext("2d");
-ctx.fillRect(20, 20, 150, 100);
