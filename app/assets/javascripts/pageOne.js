6c6f051b057fb63b2f5defae038c7cf7ad30b591
ercekal <ercekalabalikoglu@gmail.com>
2016-10-12 17:01:54 +0100

    resolves variable restrictions

--- app/assets/javascripts/pageOne.js
+++ app/assets/javascripts/pageOne.js
@@ -21,23 +21,47 @@ function loadPageOne() {
     draw();
 
     $width.addEventListener("keyup", function(){
-      width=this.value;
-      draw();
+      if ((Number(x) + Number(this.value)) > 550) {
+        $width.value = 0;
+        distance1 = (550 - x);
+        alert('Please enter a number larger than 0 smaller than ' + distance1);
+      } else {
+        width=this.value;
+        draw();
+    }
     });
 
     $height.addEventListener("keyup", function(){
+      if ((Number(y) + Number(this.value)) > 500) {
+        $height.value = 0;
+        distance2 = (500 - y);
+        alert('Please enter a number larger than 0 smaller than ' + distance2);
+      } else {
       height=this.value;
       draw();
+    }
     });
 
     $xPosition.addEventListener("keyup", function(){
+      if ((Number(width) + Number(this.value)) > 550) {
+        $xPosition.value = 0;
+        distance3 = (550 - width);
+        alert('Please enter a number smaller larger than 0 than ' + distance3);
+      } else {
       x=this.value;
       draw();
+    }
     });
 
     $yPosition.addEventListener("keyup", function(){
+      if ((Number(height) + Number(this.value)) > 550) {
+        $yPosition.value = 0;
+        distance4 = (550 - height);
+        alert('Please enter a number smaller larger than 0 than ' + distance4);
+      } else {
       y=this.value;
       draw();
+    }
     });
 
     function draw() {
